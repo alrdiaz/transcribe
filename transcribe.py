70d07feed9b725f12e049114806f50908f5b5fa4
alrdiaz <aromero4@eafit.edu.co>
2023-05-31 08:43:01 -0500

    solve language and error encode=utf8

--- transcribe.py
+++ transcribe.py
@@ -1,12 +1,26 @@
 import whisper
 
 import moviepy.editor as mp
-
+from datetime import datetime
 from progress.bar import ShadyBar
 from progress.spinner import Spinner
 
+
+# print start moment
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("--------------------------------------------------------------------")
+print("Start at: ", current_time)
+print("--------------------------------------------------------------------")
+
+
 # Provide the path to the video file and the language code ('es' for Spanish)
-video_path = "/home/alejandro/Vídeos/Videograbación 2023-05-04 15:35:21.mp4"
+# path in windows
+video_path = r"C:\Users\alromero\Videos\parte2.mp4"
+
+# path in linux 
+# video_path = "/home/alejandro/Vídeos/Videograbación 2023-05-04 15:35:21.mp4"
+
 language_code = "es"
 
 my_clip = mp.VideoFileClip(video_path)
@@ -36,7 +50,7 @@ print("--------------------------------------------------------------------")
 # decode the audio
 spinner2 = Spinner("Decoding audio ")
 spinner2.next()
-options = whisper.DecodingOptions(fp16=False)
+options = whisper.DecodingOptions(language=language_code,fp16=False)
 result = whisper.decode(model, mel, options)
 
 
@@ -52,10 +66,22 @@ segmentsRange = len(result["segments"])
 
 bar = ShadyBar("Processing text segments", max=segmentsRange, suffix="%(percent)d%%")
 for sentence in result["segments"]:
+    hoursStart=sentence["start"]
+    hhStart=str(hoursStart/3600).split(".")[0]
+    minutesStart=float("."+(str(hoursStart/3600).split(".")[1]))*60
+    mmStart=str(minutesStart).split(".")[0]
+    ssStart=str(round(float("."+(str(minutesStart).split(".")[1]))*60))
+    
+    hoursEnd=sentence["end"]
+    hhEnd=str(hoursEnd/3600).split(".")[0]
+    minutesEnd=float("."+(str(hoursEnd/3600).split(".")[1]))*60
+    mmEnd=str(minutesEnd).split(".")[0]
+    ssEnd=str(round(float("."+(str(minutesEnd).split(".")[1]))*60))
+    
     timestamps.append(
-        str(sentence["start"])
+        str(hhStart+":"+mmStart+":"+ssStart)
         + " - "
-        + str(sentence["end"])
+        + str(hhEnd+":"+mmEnd+":"+ssEnd)
         + ":"
         + str(sentence["text"])
     )
@@ -68,7 +94,7 @@ print("--------------------------------------------------------------------")
 
 # # Write the transcript to a text file
 timestampsRange = len(timestamps)
-with open("transcript.txt", "w") as file:
+with open("transcript.txt", "w", encoding='utf-8') as file:
     bar2 = ShadyBar("Processing text file", max=timestampsRange, suffix="%(percent)d%%")
     for timestamp in timestamps:
         file.write(f"{timestamp}\n")
@@ -77,3 +103,9 @@ bar2.finish()
 
 print("--------------------------------------------------------------------")
 print("Transcription saved to transcript.txt")
+# print start moment
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("--------------------------------------------------------------------")
+print("Finish at: ", current_time)
+print("--------------------------------------------------------------------")
