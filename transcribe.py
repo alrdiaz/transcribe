ef702bb7ec1c42058bdfa08b8f1c4c17a8cabb60
alrdiaz <aromero4@eafit.edu.co>
2023-12-13 15:42:16 -0500

    v2

--- transcribe.py
+++ transcribe.py
@@ -16,9 +16,9 @@ print("--------------------------------------------------------------------")
 
 # Provide the path to the video file and the language code ('es' for Spanish)
 # path in windows
-video_path = r"C:\Users\alromero\Videos\parte2.mp4"
+video_path = r"C:\Users\alromero\Videos\AFINIA - Seguimiento Programa Alta Tensión (2023-12-11 10_15 GMT-5).mp4"
 
-# path in linux 
+# path in linux
 # video_path = "/home/alejandro/Vídeos/Videograbación 2023-05-04 15:35:21.mp4"
 
 language_code = "es"
@@ -28,7 +28,15 @@ my_clip = mp.VideoFileClip(video_path)
 my_clip.audio.write_audiofile(r"my_result.mp3")
 
 
-model = whisper.load_model("medium")
+# |  Size  | Parameters | English-only model | Multilingual model | Required VRAM | Relative speed |
+# |:------:|:----------:|:------------------:|:------------------:|:-------------:|:--------------:|
+# |  tiny  |    39 M    |     `tiny.en`      |       `tiny`       |     ~1 GB     |      ~32x      |
+# |  base  |    74 M    |     `base.en`      |       `base`       |     ~1 GB     |      ~16x      |
+# | small  |   244 M    |     `small.en`     |      `small`       |     ~2 GB     |      ~6x       |
+# | medium |   769 M    |    `medium.en`     |      `medium`      |     ~5 GB     |      ~2x       |
+# | large  |   1550 M   |        N/A         |      `large`       |    ~10 GB     |       1x       |
+
+model = whisper.load_model("large")
 # load audio and pad/trim it to fit 30 seconds
 
 spinner = Spinner("Loading model to audio ")
@@ -50,7 +58,7 @@ print("--------------------------------------------------------------------")
 # decode the audio
 spinner2 = Spinner("Decoding audio ")
 spinner2.next()
-options = whisper.DecodingOptions(language=language_code,fp16=False)
+options = whisper.DecodingOptions(language=language_code, fp16=False)
 result = whisper.decode(model, mel, options)
 
 
@@ -66,22 +74,22 @@ segmentsRange = len(result["segments"])
 
 bar = ShadyBar("Processing text segments", max=segmentsRange, suffix="%(percent)d%%")
 for sentence in result["segments"]:
-    hoursStart=sentence["start"]
-    hhStart=str(hoursStart/3600).split(".")[0]
-    minutesStart=float("."+(str(hoursStart/3600).split(".")[1]))*60
-    mmStart=str(minutesStart).split(".")[0]
-    ssStart=str(round(float("."+(str(minutesStart).split(".")[1]))*60))
-    
-    hoursEnd=sentence["end"]
-    hhEnd=str(hoursEnd/3600).split(".")[0]
-    minutesEnd=float("."+(str(hoursEnd/3600).split(".")[1]))*60
-    mmEnd=str(minutesEnd).split(".")[0]
-    ssEnd=str(round(float("."+(str(minutesEnd).split(".")[1]))*60))
-    
+    hoursStart = sentence["start"]
+    hhStart = str(hoursStart / 3600).split(".")[0]
+    minutesStart = float("." + (str(hoursStart / 3600).split(".")[1])) * 60
+    mmStart = str(minutesStart).split(".")[0]
+    ssStart = str(round(float("." + (str(minutesStart).split(".")[1])) * 60))
+
+    hoursEnd = sentence["end"]
+    hhEnd = str(hoursEnd / 3600).split(".")[0]
+    minutesEnd = float("." + (str(hoursEnd / 3600).split(".")[1])) * 60
+    mmEnd = str(minutesEnd).split(".")[0]
+    ssEnd = str(round(float("." + (str(minutesEnd).split(".")[1])) * 60))
+
     timestamps.append(
-        str(hhStart+":"+mmStart+":"+ssStart)
+        str(hhStart + ":" + mmStart + ":" + ssStart)
         + " - "
-        + str(hhEnd+":"+mmEnd+":"+ssEnd)
+        + str(hhEnd + ":" + mmEnd + ":" + ssEnd)
         + ":"
         + str(sentence["text"])
     )
@@ -94,7 +102,7 @@ print("--------------------------------------------------------------------")
 
 # # Write the transcript to a text file
 timestampsRange = len(timestamps)
-with open("transcript.txt", "w", encoding='utf-8') as file:
+with open("transcript.txt", "w", encoding="utf-8") as file:
     bar2 = ShadyBar("Processing text file", max=timestampsRange, suffix="%(percent)d%%")
     for timestamp in timestamps:
         file.write(f"{timestamp}\n")
